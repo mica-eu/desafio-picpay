399c4721fe69b00c6b0e2e408f51cca459d8d4b3
micaelsouza <micael.souza@outlook.com>
2018-09-27 21:07:52 -0300

    Interceptando erros de autenticação nas requisições

--- web/src/api.js
+++ web/src/api.js
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+import router from './router';
 
 const axios = Axios.create({ baseURL: 'http://localhost:8000' });
 
@@ -7,5 +8,14 @@ axios.interceptors.request.use((config) => {
   if (accessToken) config.headers.common['Authorization'] = `Bearer ${accessToken}`;
   return config;
 }, (error) => Promise.reject(error));
+
+axios.interceptors.response.use((config) => config, (error) => {
+  if (error.response.status === 401) {
+    console.log('unauthorized, logging out ...');
+    localStorage.clear();
+    router.replace('/login');
+  }
+  return Promise.reject(error);
+});
 
 export default axios;
